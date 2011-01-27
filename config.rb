aeb80c658cb1ddbd330d43df181c27535453e0bf
cmoylan <chris@chrismoylan.com>
2011-01-27 00:41:16 -0600

    added simple config file

---
+++ config.rb
@@ -0,0 +1,13 @@
+#!/usr/bin/env ruby
+
+# TODO fill this in
+$config = {
+  :server => '',
+  :port => 0,
+  :channel => '',
+  :nick => '',
+  :password => nil,
+  :gaccount => '',
+  :gpassword => '',
+  :number => ''
+}
