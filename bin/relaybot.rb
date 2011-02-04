99f457b8103cd076e92ff91f50d031d04e16ac15
cmoylan <chris@chrismoylan.com>
2011-02-04 01:52:17 -0600

    messing around with the launcher

--- bin/relaybot.rb
+++ bin/relaybot.rb
@@ -1,11 +1,22 @@
 #!/usr/bin/env ruby
-#
+
 
 $LOAD_PATH << File.dirname(__FILE__) + '/../lib'
-require File.dirname(__FILE__) + '/../config.rb'
+require File.dirname(__FILE__) + '/../config2.rb'
 require 'relaybot.rb'
 
-bot = RelayBot.new $CONFIG
+# TODO: get command line arguments
+
+pid = Process.fork do
+  the_bot = RelayBot.new $CONFIG
+  trap("QUIT") {the_bot.quit; exit}
+end
+
+Process.detach(pid)
+# TODO: store the pid somewhere.
+#
+#
+#
 #PID_FILE = File.dirname(__FILE__) + '/../tmp/pid.txt'
 
 #if File.exist?(PID_FILE)
